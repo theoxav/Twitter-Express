e55ea24dc6c87f528e8610a47ed56f493503ece6
theoxav <theoxxav@outlook.fr>
2024-08-13 17:16:13 +0200

    Add not found page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const app = express();
-const index = require("./routes");
+const routes = require("./routes");
 
 const connectDB = require("./database/connection");
 
@@ -15,6 +15,10 @@ app.use(morgan("short"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(index);
+
+app.use(routes);
+app.use("*", (req, res) => {
+  res.status(404).render("not-found");
+});
 
 module.exports = app;
