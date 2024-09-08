b5cb10d6a7bd65664d8c48a27d1ba25c09da39c5
theoxav <theoxxav@outlook.fr>
2024-09-08 12:20:40 +0200

    Add Email verification with mailtrap and nodemailer after registration

--- config/config.js
+++ config/config.js
@@ -7,9 +7,19 @@ const MONGODB_URI =
     : process.env.MONGODB_URI;
 
 const SESSION_SECRET = process.env.SESSION_SECRET;
+const EMAIL_FROM = process.env.EMAIL_FROM;
+const MAILTRAP_HOST = process.env.MAILTRAP_HOST;
+const MAILTRAP_PORT = process.env.MAILTRAP_PORT;
+const MAILTRAP_USER = process.env.MAILTRAP_USER;
+const MAILTRAP_PASS = process.env.MAILTRAP_PASS;
 
 module.exports = {
   MONGODB_URI,
   PORT,
   SESSION_SECRET,
+  EMAIL_FROM,
+  MAILTRAP_HOST,
+  MAILTRAP_PORT,
+  MAILTRAP_USER,
+  MAILTRAP_PASS,
 };
